371b5e7c012f63c21aa0160296a050754c33c89f
Marco Giacomello <m.giacomello@insiel.it>
2022-12-27 15:54:41 +0100

    first commit

---
+++ src/main/java/com/marco/ultimaeureka/UltimaEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.marco.ultimaeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UltimaEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UltimaEurekaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marco/ultimaeureka/UltimaEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marco.ultimaeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UltimaEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
